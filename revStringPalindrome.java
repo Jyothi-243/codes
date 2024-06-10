a5bb020961db2dc168bd2376c1b242a89a724a16
Jyothi-243 <priyavankayalapati243@gmail.com>
2024-06-10 20:47:59 +0530

    reverse of a string is added

---
+++ revStringPalindrome.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * revString
+ */
+public class revStringPalindrome {
+    public  boolean isReverse(String s){
+        String s1=s;
+        String s2="";
+        for(int i=0;i<s1.length();i++){
+            s2=s1.charAt(i)+s2;
+        }
+        if(s1.equals(s2)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        revStringPalindrome obj1=new revStringPalindrome();
+        boolean res=obj1.isReverse(s);
+        if(res==true){
+            System.out.println("given string is palindrome");
+        }
+        else{
+            System.out.println("given string is not a palindrome");
+        }
+        
+    }
+    
+}
