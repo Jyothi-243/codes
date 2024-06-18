946cac34bcd8d36f79e2b9468cfcdd319f227a2c
Jyothi-243 <priyavankayalapati243@gmail.com>
2024-06-18 10:12:04 +0530

    number palindrome code

---
+++ numpalindrome14.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class numpalindrome14
+{   
+    public static boolean isPalindrome(int num){
+        int temp=num;
+        int sum=0;
+        int rev;
+        if(num<0){
+            
+            return false;
+        }
+        else{
+            while(num>0){
+                rev=num%10;
+                sum=sum*10+rev;
+                num=num/10;
+            }
+            if(sum==temp){
+                return true;
+            }
+            else{
+               return false;
+            }
+        }
+    }
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.print("enter your number: ");
+	    int num=sc.nextInt();
+		boolean b=isPalindrome(num);
+		System.out.println(b);
+	}
+}
