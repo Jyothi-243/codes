a812cb17446d700341624581e6787b2db1f3c1f0
Jyothi-243 <priyavankayalapati243@gmail.com>
2024-06-18 18:17:00 +0530

    length of last word

---
+++ lastword16.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class lastword16
+{   
+    public static int lengthOfLastWord(String strs){
+        strs=strs.trim();
+        String[] arr=strs.split(" ");
+        int size=arr.length;
+         String lastWord=arr[size-1];
+         return lastWord.length();
+    }
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.print("Enter the string: ");
+	    String strs=sc.nextLine();
+		int res=lengthOfLastWord(strs);
+		System.out.println(res);
+	}
+}
